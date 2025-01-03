22e8f013b66a0a6ed8729118d1a3f0a1acd54436
Roshan Kumar <roshaen09@gmail.com>
2025-01-03 18:30:01 +0530

    Added flake-utils

--- flake.nix
+++ flake.nix
@@ -3,32 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: 
-  let
-    system = "aarch64-darwin";
-    name = "FlexAuth";
-    src = ./.;
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    packages.${system}.default = derivation {
-      
-      inherit name system src;
-      
-      buildInputs = with pkgs; [
-        rust
-        cargo
-      ];
+  outputs = { self, nixpkgs, flake-utils}: 
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      system = "aarch64-darwin";
+      name = "FlexAuth";
+      src = ./.;
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = derivation {
+        inherit name system src;
+        
+        buildInputs = with pkgs; [
+          rust
+          cargo
+        ];
 
-      phases = [ "buildPhase" ];
-      buildPhase = ''
-        CARGO_TARGET_DIR=$out ${nixpkgs.cargo}/bin/cargo build --manifest-path $src/Cargo.toml --release 
-        ${nixpkgs.binutils}/bin/strip $out/release/flakes -o /tmp/test
-        rm -rf $out
-        mv /tmp/test $out
-      '';
-    };
-  };
+        phases = [ "buildPhase" ];
+        buildPhase = ''
+          CARGO_TARGET_DIR=$out ${nixpkgs.cargo}/bin/cargo build --manifest-path $src/Cargo.toml --release 
+          ${nixpkgs.binutils}/bin/strip $out/release/flakes -o /tmp/test
+          rm -rf $out
+          mv /tmp/test $out
+        '';
+      };
+    }
+  );
 }
